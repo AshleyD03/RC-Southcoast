1e8ecd77822ee2298707757a5ea2aafcbec32884
Ashley <adoel3507@gmail.com>
2021-03-31 08:58:25 +0100

    Created clone alert element

--- resources/modules/Record.js
+++ resources/modules/Record.js
@@ -14,15 +14,25 @@ class Record {
         this._node = document.getElementById('penalty-record-template').content.cloneNode(true).children[0];
         this._nodeTime = this._node.children[0];
         this._nodeMsg = this._node.children[1];
+        this._alertNode = null;
 
         // Apply styling
         this._nodeTime.innerHTML = this._time;
         this._nodeMsg.innerHTML = this._msg;
+
+        this._createAlert_();
     }
 
     _createAlert_ () {
         let target = document.getElementById('record-alert-target');
+        this._alertNode = document.getElementById('penalty-alert-template').cloneNode(true).content.children[0];
+        let title = this._alertNode.children[0];
+        let reset = this._alertNode.children[1];
 
+        title.innerHTML = this._msg;
+        Array.from(target).forEach(tar => tar.style.marginBottom = '1rem');
+        this._alertNode.style.marginBottom = '4rem'
+        target.appendChild(this._alertNode)
     }
 
     _removeNode_ () {
